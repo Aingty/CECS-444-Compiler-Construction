cdc089e48822dac299e1c2c691bd45cafd638eb3
Aingty Eung <aingtyeung@yahoo.com>
2018-11-15 20:29:37 -0800

    Working on Main

--- Project-2-Parser/src/Main.java
+++ Project-2-Parser/src/Main.java
@@ -15,7 +15,50 @@ public class Main
 {
 	public static void main (String args[])
 	{
-		Rule parser = new Rule();
-		System.out.println(parser.getRule(53));
+		Queue<String> queue = new LinkedList<String>();
+		Stack<String> stack = new Stack<String>();
+
+		Lexicon lexer = new Lexicon();
+		Parser parser = new Parser();
+
+		String lineTest = "prog main { ";
+		ArrayList<String> parsedString = lexer.parser(lineTest);
+
+		stack.push("Pgm");
+
+		// Generate the rule table for parser
+		parser.generateRules();
+		
+		for (int i = 0; i < parsedString.size(); i++)
+		{
+			queue.add(parsedString.get(i));
+		}
+
+		while (!stack.empty())
+		{
+			// M1
+			if (lexer.getToken(stack.peek()) == lexer.getToken(queue.peek()))
+			{
+				if (lexer.getToken(stack.peek()) == 0) // Top == $
+				{
+					System.out.println("WIN!!!");
+				}
+				else
+				{
+					stack.pop();
+					queue.remove();
+				}
+			}
+			// M2E
+			if (lexer.getToken(stack.peek()) != 99)
+			{
+				System.out.println("ERROR!!!: TOP of Stack is a TERMINAL, that doesn't match FRONT of Queue (M2)");
+				System.exit(0);
+			}
+			// M3E : We don't need to do this becasue our cell is never EMPTY!!!!
+			
+			// M4
+			
+		}
 	}
 }
