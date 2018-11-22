3df107fedad054bf6690fd43b66b1d5875dbdce3
Aingty Eung <aingtyeung@yahoo.com>
2018-11-22 02:00:51 -0800

    Main in progress

--- Project-2-Parser/src/Main.java
+++ Project-2-Parser/src/Main.java
@@ -15,25 +15,35 @@ public class Main
 {
 	public static void main (String args[])
 	{
+		// Stack and Queue used for the Program 
 		Queue<String> queue = new LinkedList<String>();
 		Stack<String> stack = new Stack<String>();
 
+		// Lexicon used from project 1 and new Parser
 		Lexicon lexer = new Lexicon();
 		Parser parser = new Parser();
 
 		String lineTest = "prog main { ";
+
+		// @parsedString is used to get an arraylist of word in a line
 		ArrayList<String> parsedString = lexer.parser(lineTest);
 
+		// @nodeList is used to keep track of the AST
+		ArrayList<Node> nodeList = new ArrayList<Node>();
+
+		// Always the intial push to the Stack
 		stack.push("Pgm");
 
 		// Generate the rule table for parser
 		parser.generateRules();
 		
+		// Adding the whole arraylist of a line into the Queue
 		for (int i = 0; i < parsedString.size(); i++)
 		{
 			queue.add(parsedString.get(i));
 		}
 
+		// LL Parser Mechanic
 		while (!stack.empty())
 		{
 			// M1
@@ -49,15 +59,21 @@ public class Main
 					queue.remove();
 				}
 			}
+			
 			// M2E
-			if (lexer.getToken(stack.peek()) != 99)
+			else if (lexer.getToken(stack.peek()) != 99)
 			{
 				System.out.println("ERROR!!!: TOP of Stack is a TERMINAL, that doesn't match FRONT of Queue (M2)");
 				System.exit(0);
 			}
+
 			// M3E : We don't need to do this becasue our cell is never EMPTY!!!!
 			
 			// M4
+			else
+			{
+				
+			}
 			
 		}
 	}
