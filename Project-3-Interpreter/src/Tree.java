e0beb545d0aa959074a57898e3970bc8529a72c3
Aingty Eung <aingtyeung@yahoo.com>
2018-12-04 20:50:29 -0800

    Work in prgress Erase this if needed

---
+++ Project-3-Interpreter/src/Tree.java
@@ -0,0 +1,31 @@
+/*
+    Team VOS:                   Email:
+        Sebastian Adame         sjadamem@gmail.com
+        Darian Bezayiff         darrianbez@gmail.com
+        Aingty Eung             aingtyeung@yahoo.com
+        Angela Flores           Angfl97@gmail.com
+        Matthew Nguyen          matthewvietnguyen@gmail.com
+*/
+import java.util.*;
+
+public class Tree
+{
+    private Node root;
+    
+    public Tree()
+    {
+        root = null;
+    }
+
+    public boolean add(Node child, int parentID)
+    {
+        if (root == null)
+        {
+            root = child;
+        }
+        else
+        {
+            
+        }
+    }
+}
